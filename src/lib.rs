6d496fa8dd62010e3060b360b39fe5b3f36c4691
Zachary Dremann <dremann@gmail.com>
2022-06-02 14:41:43 -0400

    Prep for moving to new edition

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ mod rolling_hash;
 mod separator;
 mod tree;
 
-pub use chunk::{Chunk, ChunkIter};
-pub use polynom::{Polynom, Polynom64};
-pub use rolling_hash::{Rabin64, RollingHash64};
-pub use separator::{HashToLevel, Separator, SeparatorIter};
-pub use tree::{HashedChunk, Node, NodeIter};
+pub use crate::chunk::{Chunk, ChunkIter};
+pub use crate::polynom::{Polynom, Polynom64};
+pub use crate::rolling_hash::{Rabin64, RollingHash64};
+pub use crate::separator::{HashToLevel, Separator, SeparatorIter};
+pub use crate::tree::{HashedChunk, Node, NodeIter};
