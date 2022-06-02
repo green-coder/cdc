5306bce182de43869fa7bc4151996fb211102159
Zachary Dremann <dremann@gmail.com>
2022-06-02 16:08:29 -0400

    Group benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,16 +1,21 @@
 use cdc::{Rabin64, RollingHash64};
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId, black_box, Throughput};
 
 pub fn slide_benchmarks(c: &mut Criterion) {
-    for i in [1_000, 10_000, 100_000] {
-        c.bench_function(&format!("slide {}x", i), |b| {
-            let data: u8 = 16; //arbitrary value
-            b.iter(|| {
-                let mut rabin = Rabin64::new(5);
-                for _ in 0..i {
-                    rabin.slide(&data)
-                }
-            })
+    let mut group = c.benchmark_group("slide");
+    let data = 16;
+    for size in [1_000, 10_000, 100_000] {
+        group.throughput(Throughput::Bytes(size));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            b.iter_batched(
+                || Rabin64::new(5),
+                |mut rabin| {
+                    for _ in 0..size {
+                        rabin.slide(black_box(&data));
+                    }
+                },
+                criterion::BatchSize::SmallInput,
+            );
         });
     }
 }
