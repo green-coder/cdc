c17568969a180541a805c12f474512a138d42972
Zachary Dremann <dremann@gmail.com>
2022-07-05 21:29:35 -0400

    Add tests for creating a new Rabin64

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,7 +1,7 @@
 use cdc::{Rabin64, RollingHash64};
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId, black_box, Throughput};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
-pub fn slide_benchmarks(c: &mut Criterion) {
+fn slide_benchmarks(c: &mut Criterion) {
     let mut group = c.benchmark_group("slide");
     let data = 16;
     for size in [1_000, 10_000, 100_000] {
@@ -20,5 +20,15 @@ pub fn slide_benchmarks(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, slide_benchmarks);
+fn create_benchmarks(c: &mut Criterion) {
+    c.bench_function("new", |b| {
+        b.iter(|| Rabin64::new(5));
+    });
+
+    c.bench_function("with_polynom", |b| {
+        b.iter(|| Rabin64::new_with_polynom(5, &0x3847fe406c36e1));
+    });
+}
+
+criterion_group!(benches, slide_benchmarks, create_benchmarks);
 criterion_main!(benches);
